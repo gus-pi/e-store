9f7fc3fdb0cc3ee0450172e72b783b86d0424690
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-12-21 12:25:10 -0400

    fill product table

--- frontend/src/pages/admin/products/ProductList.tsx
+++ frontend/src/pages/admin/products/ProductList.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+export type Product = {
+  id: number;
+  name: string;
+  brand: string;
+  category: string;
+  price: 1199;
+  description: string;
+  imageFilename: string;
+  createdAt: string;
+};
+
 const ProductList = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  async function getProducts() {
+    try {
+      const response = await fetch(
+        'http://localhost:4000/products?_sort=id&_order=desc'
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setProducts(data);
+      }
+    } catch (error) {
+      alert('Unable to fetch data');
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="container my-4">
       <h2 className="text-center mb-4">Products</h2>
@@ -9,7 +41,11 @@ const ProductList = () => {
           <Link className="btn btn-primary me-1" to="/admin/products/create">
             Create Product
           </Link>
-          <button type="button" className="btn btn-outline-primary">
+          <button
+            onClick={getProducts}
+            type="button"
+            className="btn btn-outline-primary"
+          >
             Refresh
           </button>
         </div>
@@ -28,7 +64,36 @@ const ProductList = () => {
             <th>Action</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>{product.brand}</td>
+              <td>{product.category}</td>
+              <td>${product.price}</td>
+              <td>
+                <img
+                  src={`http://localhost:4000/images/${product.imageFilename}`}
+                  alt="product"
+                  width="100"
+                />
+              </td>
+              <td>{product.createdAt.slice(0, 10)}</td>
+              <td style={{ width: '10px', whiteSpace: 'nowrap' }}>
+                <Link
+                  className="btn bt-primary btn-sm me-1"
+                  to={`/admin/products/edit/${product.id}`}
+                >
+                  Edit
+                </Link>
+                <button type="button" className="btn btn-danger btn-sm">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
