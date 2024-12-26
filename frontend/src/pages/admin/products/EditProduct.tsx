e3b7f0cdb4319cdcb9d0358de54258b5ac96fa67
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-12-26 16:15:36 -0400

    add edit product page

---
+++ frontend/src/pages/admin/products/EditProduct.tsx
@@ -0,0 +1,183 @@
+import { useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+type ErrorType = {
+  name: string;
+  brand: string;
+  category: string;
+  price: string;
+  description: string;
+  image: string;
+};
+
+const EditProduct = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const [validationErrors, setValidationErrors] = useState<ErrorType>();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const product = Object.fromEntries(formData.entries());
+    if (
+      !product.name ||
+      !product.brand ||
+      !product.category ||
+      !product.price ||
+      !product.description
+    ) {
+      alert('Please fill all fields!');
+      return;
+    }
+    try {
+      const response = await fetch('http://localhost:4000/products', {
+        method: 'PATCH',
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        navigate('/admin/products');
+      } else if (response.status === 400) {
+        setValidationErrors(data);
+      } else {
+        alert('Unable to fetch products');
+      }
+    } catch (error) {
+      alert('Unable to connect to the server');
+    }
+  };
+  return (
+    <div className="container my-4">
+      <div className="row">
+        <div className="col-md-8 mx-auto rounded border p-4">
+          <h2 className="text-center mb-5">Edit Product</h2>
+
+          <div className="row mb-3">
+            <label className="col-sm-4 col-form-label">ID</label>
+            <div className="col-sm-8">
+              <input
+                readOnly
+                className="form-control-plaintext"
+                defaultValue={params.id}
+              />
+            </div>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Name</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="name" />
+                <span className="text-danger">{validationErrors?.name}</span>
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Brand</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="brand" />
+                <span className="text-danger">{validationErrors?.brand}</span>
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Category</label>
+              <div className="col-sm-8">
+                <select className="form-select" name="category">
+                  <option value="Other">Other</option>
+                  <option value="Phones">Phones</option>
+                  <option value="Computers">Computers</option>
+                  <option value="Accessories">Accessories</option>
+                  <option value="Printers">Printers</option>
+                  <option value="Cameras">Cameras</option>
+                </select>
+                <span className="text-danger">
+                  {validationErrors?.category}
+                </span>
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Price</label>
+              <div className="col-sm-8">
+                <input
+                  className="form-control"
+                  name="price"
+                  type="number"
+                  step="0.01"
+                  min="1"
+                />
+                <span className="text-danger">{validationErrors?.price}</span>
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Description</label>
+              <div className="col-sm-8">
+                <textarea
+                  className="form-control"
+                  name="description"
+                  rows={4}
+                />
+                <span className="text-danger">
+                  {validationErrors?.description}
+                </span>
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-sm-8 offset-sm-4">
+                <img
+                  src={`http://localhost:4000/images/${'10590390.jpg'}`}
+                  alt="image"
+                  width={150}
+                />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Image</label>
+              <div className="col-sm-8">
+                <input className="form-control" type="file" name="image" />
+                <span className="text-danger">{validationErrors?.image}</span>
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Created at</label>
+              <div className="col-sm-8">
+                <input
+                  className="form-control-plaintext"
+                  readOnly
+                  defaultValue={'2024-26-12'}
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="offset-sm-4 col-sm-4 d-grid">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+              <div className="col-sm-4 d-grid">
+                <Link
+                  className="btn btn-secondary"
+                  to="/admin/products"
+                  role="button"
+                >
+                  Cancel
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditProduct;
