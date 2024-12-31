c7088f42ad836421ff36c2c52efe66c4eae996e1
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-12-31 12:59:38 -0400

    create the register user component

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,3 @@
-import { Button, Container } from 'react-bootstrap';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,5 +1,124 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let formData = new FormData(e.currentTarget);
+    let user = Object.fromEntries(formData.entries());
+
+    if (!user.firstname || !user.lastname || !user.email || !user.password) {
+      alert('Please fill all the required fields');
+      return;
+    }
+    if (user.password !== user.confirm_password) {
+      alert('Passwords must match');
+      return;
+    }
+    delete user.confirm_password;
+
+    try {
+      const response = await fetch('http://localhost:4000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        console.log('server response: ', data);
+        navigate('/');
+      } else {
+        alert('Unable to register' + data);
+      }
+    } catch (error) {
+      alert('Unable to connect to server');
+    }
+  };
+  return (
+    <div className="container my-4">
+      <div className="row">
+        <div className="col-lg-8 mx-auto rounded border p-4">
+          <h2 className="text-center mb-5">Create new Account</h2>
+
+          <form onSubmit={handleSubmit}>
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">First Name *</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="firstname" />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Last Name *</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="lastname" />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Email *</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="email" />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Phone</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="phone" />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Address</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="address" />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Password *</label>
+              <div className="col-sm-8">
+                <input
+                  className="form-control"
+                  type="password"
+                  name="password"
+                />
+              </div>
+            </div>
+
+            <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">
+                Confirm Password *
+              </label>
+              <div className="col-sm-8">
+                <input
+                  className="form-control"
+                  type="password"
+                  name="confirm_password"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="offset-sm-4 col-sm-4 d-grid">
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+              </div>
+              <div className="col-sm-4 d-grid">
+                <Link className="btn btn-outline-primary" to="/" role="button">
+                  Cancel
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
